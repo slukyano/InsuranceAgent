5f02021188462ff20b70c2981860659c2d296ec7
Никита <nikitos1810@gmail.com>
2013-11-18 23:49:58 +0400

    sql improved

--- sql/roles.sql
+++ sql/roles.sql
@@ -1,14 +1,17 @@
+--ADMINS--
 drop user insurance_admin;
 drop role insurance_admins;
-
 
 create user insurance_admin identified by 4321;
 create role insurance_admins;
 
 grant insurance_admins to insurance_admin;
+
 grant all privileges to insurance_admins with admin option;
+
 alter user insurance_admin default role insurance_admins;
 
+--MANAGERS--
 drop user insurance_manager;
 drop role insurance_managers;
 
@@ -16,13 +19,58 @@ create user insurance_manager identified by 4321;
 create role insurance_managers;
 
 grant insurance_managers to insurance_manager;
-grant select any table,
-      insert any table,
-      update any table,
-      delete any table 
-      to insurance_managers;
-alter user insurance_manager default role insurance_managers;
 
+grant select,
+      insert,
+      update,
+      delete
+      on AGENTS to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on Attribute_types to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on companies to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on companies_by_insurance_type to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on insurance_attributes to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on Insurance_types to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on insurances to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on legal_persons to insurance_managers;
+grant select,
+      insert,
+      update,
+      delete
+      on natural_persons to insurance_managers;
+grant select on USERS_AND_USERDATA to insurance_managers;
+grant create session to insurance_managers;
+
+
+alter user insurance_manager default role insurance_managers;
+--AGENTS--
 drop user insurance_agent;
 drop role insurance_agents;
 
@@ -35,25 +83,74 @@ grant select,
       insert,
       update,
       delete 
-      on LEGAL_PERSONS 
-      to insurance_agents;
+      on LEGAL_PERSONS to insurance_agents;
 grant select,
       insert,
       update,
       delete 
-      on NATURAL_PERSONS 
-      to insurance_agents;
+      on NATURAL_PERSONS to insurance_agents;
 grant select,
       insert,
       update,
       delete 
       on INSURANCES to insurance_agents;
+grant select,
+      insert,
+      update,
+      delete
+      on insurance_attributes to insurance_agents;
+grant select on AGENTS to insurance_agents;
+grant select on Attribute_types to insurance_agents;
+grant select on companies to insurance_agents;
+grant select on companies_by_insurance_type to insurance_agents;
+grant select on Insurance_types to insurance_agents;
+grant select on insurances to insurance_agents;
+grant select on legal_persons to insurance_agents;
+grant select on natural_persons to insurance_agents;
 grant select on USERS_AND_USERDATA to insurance_agents;
+grant create session to insurance_agents;
       
 alter user insurance_agent default role insurance_agents;
+--LEGALS--
+drop user insurance_legal;
+drop role insurance_legals;
 
+create user insurance_legal identified by 4321;
+create role insurance_legals;
+
+grant insurance_legals to insurance_legal;
+
+grant select on LEGAL_PERSONS to insurance_legals;
+grant select on INSURANCES to insurance_legals;
+grant select on AGENTS to insurance_legals;
+grant select on Attribute_types to insurance_legals;
+grant select on companies to insurance_legals;
+grant select on companies_by_insurance_type to insurance_legals;
+grant select on Insurance_types to insurance_legals;
+grant select on insurances to insurance_legals;
+grant select on USERS_AND_USERDATA to insurance_legals;
+grant create session to insurance_legals;
+
+alter user insurance_legal default role insurance_legals;
+
+--NATURALS--
+drop user insurance_natural;
+drop role insurance_naturals;
+
+create user insurance_natural identified by 4321;
+create role insurance_naturals;
+
+grant insurance_naturals to insurance_natural;
+
+grant select on NATURAL_PERSONS to insurance_naturals;
+grant select on INSURANCES to insurance_naturals;
+grant select on AGENTS to insurance_naturals;
+grant select on Attribute_types to insurance_naturals;
+grant select on companies to insurance_naturals;
+grant select on companies_by_insurance_type to insurance_naturals;
+grant select on Insurance_types to insurance_naturals;
+grant select on insurances to insurance_naturals;
 grant select on USERS_AND_USERDATA to insurance_naturals;
-grant select on USERS_AND_USERDATA to insurance_legals; 
-grant select on USERS_AND_USERDATA to insurance_agents;
-grant select on USERS_AND_USERDATA to insurance_managers;
-grant select on USERS_AND_USERDATA to insurance_admins;
+grant create session to insurance_naturals;
+
+alter user insurance_natural default role insurance_naturals;
