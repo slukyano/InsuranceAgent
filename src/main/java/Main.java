0a95c7d672bf22ebbe5210e313b952bec3efa5f1
Никита <nikitos1810@gmail.com>
2013-11-21 03:28:08 +0400

    css for breadcrumb

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import ui.UiRootController;
 
 import java.util.ArrayList;
 
 public class Main extends Application {
+    private ToolBar bc1;
+    private ToolBar bc2;
+    private ToolBar bc3;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,8 +22,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent rootView = FXMLLoader.load(getClass().getResource("/ui/UiRoot.fxml"));
+        Scene scene =  new Scene(rootView, 640, 480);
+        scene.getStylesheets().add("/ui/pages/home/resources/style.css");
 
-        primaryStage.setScene(new Scene(rootView, 640, 480));
+        primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.setTitle("Insurance");
         primaryStage.show();
--- src/main/java/ui/NavigateBarController.java
+++ src/main/java/ui/NavigateBarController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -32,6 +33,9 @@ public class NavigateBarController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instance=this;
+        breadCrumbsPane.getStyleClass().add("breadcrumbs");
+        //breadCrumbsPane.
+
         AddButton(0,"Home");
         //breadCrumbsPane.getChildren().add(new Button());
         //breadCrumbsPane.getChildren().add(new Button());
@@ -44,9 +48,21 @@ public class NavigateBarController implements Initializable {
     }
 
     public void AddButton(final int index, String text){
-        Button homeButton = new Button(text);
-        breadCrumbsPane.getChildren().add(homeButton);
-        homeButton.setOnAction(
+        Button button = new Button(text);
+        if(index == 0)
+            button.getStyleClass().addAll("item","first");
+        else if (index > 1){
+            Button previousButton =(Button)breadCrumbsPane.getChildren().get(breadCrumbsPane.getChildren().size()-1);
+            previousButton.getStyleClass().remove("last");
+            previousButton.getStyleClass().add("middle");
+            button.getStyleClass().addAll("item", "last");
+        }
+        else if(index==1){
+            button.getStyleClass().addAll("item", "last");
+        }
+        //button.setTextFill(Color.TRANSPARENT);
+        breadCrumbsPane.getChildren().add(button);
+        button.setOnAction(
                 new EventHandler<ActionEvent>() {
                     public void handle(final ActionEvent event) {
                         for (int i= breadCrumbsPane.getChildren().size()-1; i>=index+1 ;i--) {
